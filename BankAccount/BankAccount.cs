50123ab90081daa8afd471c3867bcbe451b915da
Patrik Ask <ask.patrik@hotmail.com>
2022-09-19 15:18:42 +0200

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        //Properties
+        public decimal Saldo { get; private set; }
+        public string Name { get; }
+    
+        //Constructor
+        public BankAccount(string name, decimal balance)
+        {
+            Saldo = balance;
+            Name = name;
+        }
+        //Methods
+        public void MakeDeposit(decimal amount)
+        {
+            Saldo += amount;
+        }
+        public void MakeWithdrawal(decimal amount)
+        {
+           if (amount > Saldo)           
+                Console.WriteLine("Insufficient funds, transaction" +
+                    " failed");           
+           else
+                Saldo -= amount;           
+        }
+        public void MakeTransfer(decimal transferSum, BankAccount acc)
+        { 
+            MakeWithdrawal(transferSum);                         
+            acc.MakeDeposit(transferSum);
+        }
+      
+    }  
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankAccount
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var patriksKonto = new BankAccount("Patrik", 5000);
+            var kevinsKonto = new BankAccount("Kevin", 7500);
+
+            patriksKonto.MakeTransfer(500, patriksKonto);
+            
+        }
+    }
+}
